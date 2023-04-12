3d69d3c71d7f93a39a22ac2f4c94e2a42b47bcfe
Ethan <ethan.nordahl1@gmail.com>
2023-04-11 20:01:22 -0700

    button change

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-let users = [
-    {
-        username: "David",
-        email: "david@fes.com",
-        password: "test123",
-        subscriptionStatus: "VIP",
-        discordId: "David Bragg#0001",
-        lessonsCompleted: [0, 1],
-    }
-]
-
-function register(user) {``
-    users.push(user);
+function changeTitleToRed() {
+    document.querySelector('#title').style.color = 'red'
 }
-
-register({
-    username: "than",
-    email: "ethan.nordahl1@gmail.com",
-    password: "123",
-    subscriptionStatus: "VIP",
-    discordId: "than#001",
-    lessonsCompleted: [0, 1]
-});
-
-console.log(users);
